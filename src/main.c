13d2d987c0f4eed6c47d73ebb4dc206e72515998
IoriBranford <ioribranford@gmail.com>
2022-07-30 12:41:01 -0700

    Simple test of controller input.

--- src/main.c
+++ src/main.c
@@ -1,16 +1,34 @@
 #include "draw.h"
 #include "image.h"
+#include "input.h"
 
 int main() {
+	short x = 0, y = 0;
 	short triangle[6];
 	u_char colors[9] = {
 		128, 0, 0,
 		0, 128, 0,
 		0, 0, 128
 	};
+	Controller *controller;
+
 	draw_setup(0, 320, 240);
+	input_setup();
+	controller = get_controller(0);
+
 	while (1) {
+		if (is_button_pressed(controller, BUTTON_RIGHT))
+			x++;
+		if (is_button_pressed(controller, BUTTON_LEFT))
+			x--;
+
 		draw_begin();
+
+		triangle[0] = x; triangle[1] = y;
+		triangle[2] = x + 16; triangle[3] = y+8;
+		triangle[4] = x; triangle[5] = y+16;
+		draw_triangle_gouraud(triangle, colors);
+
 		for (int i = 16; i <= 160; i += 16) {
 			triangle[0] = i; 	triangle[1] = i;
 			triangle[2] = i+64; triangle[3] = i;
