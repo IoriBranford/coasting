9ea8668d39d3d4bc0076126e57abee4fcae4171a
IoriBranford <ioribranford@gmail.com>
2022-07-31 14:53:35 -0700

    WIP track walk function.

--- src/track.c
+++ src/track.c
@@ -3,6 +3,8 @@
 #include "draw.h"
 #include "time.h"
 
+#include <abs.h>
+
 Track TRACKS[] = {
     {16, 0},
     {16, 0},
@@ -74,6 +76,55 @@ void track_setup() {
 Track* get_track(int i) {
     return &TRACKS[i];
 };
+
+void walk_tracks() {
+    // int f_speed = ONE;
+    int f_x = 0, f_y = 0;
+    int f_pos = 0;
+    int tri = 0;
+
+    int f_move = TRACKS[NUM_TRACKS-1].f_prevlen + TRACKS[NUM_TRACKS-1].f_len;
+
+    int f_len;
+
+    while (tri >= 0 && tri < NUM_TRACKS && f_move) {
+        Track *tr = &TRACKS[tri];
+        int f_trx0 = tr->f_x0;
+        int f_try0 = tr->f_y0;
+        int f_trx1 = tr->f_x0;
+        int f_try1 = tr->f_x0;
+        if (f_move > 0) {
+            f_trx1 += tr->dx * ONE;
+            f_try1 += tr->dy * ONE;
+        } else {
+            f_trx0 += tr->dx * ONE;
+            f_try0 += tr->dy * ONE;
+        }
+
+        if (f_x == f_trx0 && f_y == f_try0) {
+            f_len = tr->f_len;
+        } else {
+            int f_distx = f_trx1 - f_x;
+            int f_disty = f_try1 - f_y;
+            int f_distsq = f_distx * f_distx / ONE + f_disty * f_disty / ONE;
+            f_len = csqrt(f_distsq);
+        }
+
+        if (abs(f_move) >= f_len) {
+            f_x = f_trx1;
+            f_y = f_try1;
+            int dir = f_move >= 0 ? 1 : -1;
+            f_move -= dir*f_len;
+            f_pos += dir*f_len;
+            tri += dir;
+        } else {
+            f_x += tr->f_dirx * f_move;
+            f_y += tr->f_diry * f_move;
+            f_pos += f_move;
+            f_move = 0;
+        }
+    }
+}
 
 void draw_tracks(short offsetx, short offsety) {
     int cn = sizeof(COLORCYCLE);
