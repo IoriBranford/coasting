010f38ff7cd1d2ce35bb9b6e233dbf5ea29ef069
IoriBranford <ioribranford@gmail.com>
2022-08-01 14:23:46 -0700

    Don't expose old move method (but keep for reference).

--- src/track.h
+++ src/track.h
@@ -11,7 +11,6 @@ struct Track {
 typedef struct Track Track;
 
 void track_setup();
-void move_on_tracks(int *f_x, int *f_y, int *f_pos, int *tri, int *f_angle, int f_speed);
 void move_on_track(int *trackidx, int *f_position, int f_speed);
 void track_set_transform(int *f_x, int *f_y, int *f_angle, int tri, int f_pos);
 void draw_tracks(short offsetx, short offsety);
