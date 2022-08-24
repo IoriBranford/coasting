2c3b5380933dd4f1667c1bfef0a3d652c553c012
IoriBranford <ioribranford@gmail.com>
2022-08-23 22:22:31 -0700

    Try loops and spirals.

--- src/courses.h
+++ src/courses.h
@@ -1,8 +1,7 @@
 #ifndef __COURSES_H
 #define __COURSES_H
 #include "course.h"
-#define NUM_COURSES 2
+#define NUM_COURSES 1
 extern Course COURSES[];
 #define COURSE_intro 0
-#define COURSE_uphill 1
 #endif
